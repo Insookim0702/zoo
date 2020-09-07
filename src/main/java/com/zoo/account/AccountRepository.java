20f1e83372816235d7e162710c7f17ebdae218c4
insookim0702 <insookim0702@gmail.com>
2020-09-07 23:18:44 +0900

    [계정]existsByEmail()과 findByEmail() 함수 추가

--- src/main/java/com/zoo/account/AccountRepository.java
+++ src/main/java/com/zoo/account/AccountRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
+    boolean existsByEmail(String email);
+    Account findByEmail(String email);
 }
