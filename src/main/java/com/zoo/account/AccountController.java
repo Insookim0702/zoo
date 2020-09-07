79c3096b9bcd7bec9c8f84649f2036e20b3ad88c
insookim0702 <insookim0702@gmail.com>
2020-09-07 23:19:58 +0900

    [계정컨트롤러]계정 만들기 인증 구현

--- src/main/java/com/zoo/account/AccountController.java
+++ src/main/java/com/zoo/account/AccountController.java
@@ -1,25 +1,40 @@
 package com.zoo.account;
 
+import lombok.RequiredArgsConstructor;
 import org.dom4j.rule.Mode;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
+@RequiredArgsConstructor
 @Controller
 public class AccountController {
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountService accountService;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
     @GetMapping("/sign-up")
     public String signUpView(Model model){
         model.addAttribute("signUpForm", new SignUpForm());
-        return "/sign-up";
+        return "account/sign-up";
     }
 
     @PostMapping("/sign-up")
     public String signUp(@Valid SignUpForm signUpForm, Model model, Errors errors){
-        //AccountService.processSignUp(signUpForm);
+        if(errors.hasErrors()){
+            return "account/sign-up";
+        }
+        accountService.processSignUp(signUpForm);
         return "redirect:/";
     }
 }
