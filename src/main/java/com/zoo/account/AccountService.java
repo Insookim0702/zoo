34cf9283247bcb123c6f746fe41070e6893df9da
insookim0702 <insookim0702@gmail.com>
2020-09-07 23:16:07 +0900

    [개발]콘솔에 이메일 인증 토큰 출력

--- src/main/java/com/zoo/account/AccountService.java
+++ src/main/java/com/zoo/account/AccountService.java
@@ -1,8 +1,11 @@
 package com.zoo.account;
 
 import com.zoo.domain.Account;
+import javassist.Loader;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +17,21 @@ public class AccountService {
     private final AccountRepository accountRepository;
     private final PasswordEncoder passwordEncoder;
     private final ModelMapper modelMapper;
-    public void processSignUp(@Valid  SignUpForm signUpForm) {
+    private final JavaMailSender javaMailSender;
+    public void processSignUp(@Valid SignUpForm signUpForm) {
         //디비에 저장
-        saveAccountTODB(signUpForm);
+        Account account = saveAccountTODB(signUpForm);
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setSubject("동물원증 예약 시스템, 회원가입 이메일 인증");
+        simpleMailMessage.setTo(account.getEmail());
+        simpleMailMessage.setText("/check-email-token?token="+account.getEmailCheckToken()+"&email="+account.getEmail());
+        javaMailSender.send(simpleMailMessage);
         //이메일 인증 토큰 전송
         //로그인 처리
     }
 
-    private Account saveAccountTODB(@Valid  SignUpForm signUpForm) {
+    private Account saveAccountTODB(@Valid SignUpForm signUpForm) {
         signUpForm.setPassword(passwordEncoder.encode(signUpForm.getPassword()));
         Account account = modelMapper.map(signUpForm, Account.class);
         account.generateEmailCheckToken();
