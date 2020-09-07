e4de81bdffd3492bef5dcb2fae03ec8f185a1b57
insookim0702 <insookim0702@gmail.com>
2020-09-07 23:17:59 +0900

    [앱 설정]비밀번호 인코딩, ModelMapper 설정

---
+++ src/main/java/com/zoo/config/AppConfig.java
@@ -0,0 +1,26 @@
+package com.zoo.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.NameTokenizers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE)
+                .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE);
+        return modelMapper;
+    }
+}
