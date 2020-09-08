409cbd9096976fe87bfa4e3be3211a7a93727824
insookim0702 <insookim0702@gmail.com>
2020-09-08 23:35:46 +0900

    비밀번호 체크 검증 기능 추가

--- src/main/java/com/zoo/account/SignUpFormValidator.java
+++ src/main/java/com/zoo/account/SignUpFormValidator.java
@@ -18,6 +18,9 @@ public class SignUpFormValidator implements Validator {
     @Override
     public void validate(Object object, Errors errors) {
         SignUpForm signUpForm = (SignUpForm)object;
+        if(!signUpForm.getCheckPassword().equals(signUpForm.getPassword())){
+            errors.rejectValue("password","invalid.password", new Object[]{signUpForm.getPassword()}, "비밀번호가 서로 다릅니다.");
+        }
         if(accountRepository.existsByEmail(signUpForm.getEmail())){
             errors.rejectValue("email","invalid.email", new Object[]{signUpForm.getEmail()},"이미 사용 중인 이메일입니다.");
         }
