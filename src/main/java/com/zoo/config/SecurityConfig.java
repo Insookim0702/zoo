4019d8a897c3611a82803e8618951250f578d552
insookim0702 <insookim0702@gmail.com>
2020-09-10 00:57:21 +0900

    [Security]설정

--- src/main/java/com/zoo/config/SecurityConfig.java
+++ src/main/java/com/zoo/config/SecurityConfig.java
@@ -17,8 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
 
-        //http.formLogin().loginPage("/login").permitAll();
-        //http.logout().logoutSuccessUrl("/");
+        http.formLogin().loginPage("/login").permitAll();
+        http.logout().logoutSuccessUrl("/");
     }
 
     @Override
