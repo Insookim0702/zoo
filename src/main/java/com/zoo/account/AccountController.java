06497342138c7b3253dd6ed3ca3afb9254106fbe
insookim0702 <insookim0702@gmail.com>
2020-09-10 23:24:15 +0900

    회원가입, 로그인, 이메일 인증 관련 컨트롤러

--- src/main/java/com/zoo/account/AccountController.java
+++ src/main/java/com/zoo/account/AccountController.java
@@ -21,19 +21,21 @@ public class AccountController {
     private final SignUpFormValidator signUpFormValidator;
     private final AccountService accountService;
     private final AccountRepository accountRepository;
+
     @InitBinder("signUpForm")
-    public void initBinder(WebDataBinder webDataBinder){
+    public void initBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(signUpFormValidator);
     }
+
     @GetMapping("/sign-up")
-    public String signUpView(Model model){
+    public String signUpView(Model model) {
         model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
     }
 
     @PostMapping("/sign-up")
-    public String signUp(@Valid SignUpForm signUpForm, Errors errors){
-        if(errors.hasErrors()){
+    public String signUp(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
             return "account/sign-up";
         }
         Account account = accountService.processSignUp(signUpForm);
@@ -41,8 +43,8 @@ public class AccountController {
     }
 
     @GetMapping("/recheck-email")
-    public String checkEmail(@CurrentUser Account account, Model model){
-        if(!account.canSendConfirmEmail()){
+    public String checkEmail(@CurrentUser Account account, Model model) {
+        if (!account.canSendConfirmEmail()) {
             model.addAttribute("oneHourError", "한 시간 후 다시 시도해 주세요.");
             model.addAttribute("lastRequestTime", account.getEmailCheckTokenGeneratedAt());
         }
@@ -51,31 +53,26 @@ public class AccountController {
     }
 
     @GetMapping("/request-emailValidateToken")
-    public String requestEmailValidateToken(@CurrentUser Account account, Model model){
-        if(!account.canSendConfirmEmail()){
-            model.addAttribute("oneHourError", "한 시간 후 다시 시도해 주세요.");
-            model.addAttribute("lastRequestTime", account.getEmailCheckTokenGeneratedAt());
-        }else{
-            accountService.emailAuthSend(account);
-            model.addAttribute("message", "이메일이 발송되었습니다.");
-        }
+    public String requestEmailValidateToken(@CurrentUser Account account, Model model) {
+        accountService.emailAuthSend(account);
+        model.addAttribute("message", "이메일이 발송되었습니다.");
         return "account/recheck-email";
     }
 
     @GetMapping("/check-email-token")
-    public String checkEmailToken(String email, String token, Model model){
+    public String checkEmailToken(String email, String token, Model model) {
         Account account = accountRepository.findByEmail(email);
         String resultPage = "account/checked-email";
-        if(account == null){
+        if (account == null) {
             model.addAttribute("error", "wrong.email");
             return resultPage;
         }
-        if(!account.isValidToken(token)){
-            model.addAttribute("error","wrong.token");
+        if (!account.isValidToken(token)) {
+            model.addAttribute("error", "wrong.token");
             return resultPage;
         }
         accountService.completeSignUp(account);
-        model.addAttribute("name",account.getName());
+        model.addAttribute("name", account.getName());
         model.addAttribute("numberOfUser", accountRepository.count());
         return resultPage;
     }
