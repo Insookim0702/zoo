2e7f07e599db1358a64dad11dd12f031d14c8859
insookim0702 <insookim0702@gmail.com>
2020-09-14 23:13:47 +0900

    계정도메인 레포지토리

--- src/main/java/com/zoo/account/AccountRepository.java
+++ src/main/java/com/zoo/account/AccountRepository.java
@@ -2,7 +2,6 @@ package com.zoo.account;
 
 import com.zoo.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
